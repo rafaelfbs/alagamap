762a14f45db329825a4b248149859d510946460b
Rafael Ferrari <me@rferrari.net>
2019-04-27 20:05:37 -0300

    fix amplify push script

--- tools/amplify/push.js
+++ tools/amplify/push.js
@@ -3,24 +3,17 @@ const { overwrite, exec } = require("./util");
 const AUTH_PARAMETERS_PATH = "./amplify/backend/auth/cognito6170dcc7/parameters.json";
 const API_PARAMETERS_PATH = "./amplify/backend/api/alagamap/parameters.json";
 
-const authParams = overwrite(AUTH_PARAMETERS_PATH, data => ({
+overwrite(AUTH_PARAMETERS_PATH, data => ({
   ...data,
   googleAppIdUserPool: process.env.GOOGLE_CREDENTIALS_CLIENT_ID,
   googleAppSecretUserPool: process.env.GOOGLE_CREDENTIALS_CLIENT_SECRET,
 }));
 
-const apiParams = overwrite(API_PARAMETERS_PATH, data => ({
+overwrite(API_PARAMETERS_PATH, data => ({
   ...data,
   OneSignalAppId: process.env.ONE_SIGNAL_APP_ID,
   OneSignalRestKey: process.env.ONE_SIGNAL_REST_KEY,
 }));
-
-
-
-function restoreParams() {
-  apiParams();
-  authParams();
-}
 
 function escape(p) {
   return `"${p.replace(/"/g, "\\\"")}"`;
@@ -58,9 +51,7 @@ async function main() {
     }
 
     console.log(`# Done initializing Amplify environment: ${ENV}`);
-    restoreParams();
   } catch (code) {
-    restoreParams();
     process.exit(code);
   }
 }
