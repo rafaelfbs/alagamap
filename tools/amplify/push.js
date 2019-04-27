603720c500763895a63af02bc6bade4d0ff14ab6
Rafael Ferrari <me@rferrari.net>
2019-04-27 13:50:03 -0300

    use auth parameters json

--- tools/amplify/push.js
+++ tools/amplify/push.js
@@ -1,38 +1,22 @@
 const { execSync } = require("child_process");
 const { overwrite } = require("./util");
 
-const PARAMETERS_PATH = "./amplify/backend/api/alagamap/parameters.json";
-const TEAM_PROVIDER_INFO_PATH = "./amplify/team-provider-info.json";
+const AUTH_PARAMETERS_PATH = "./amplify/backend/auth/cognito6170dcc7/parameters.json";
+const API_PARAMETERS_PATH = "./amplify/backend/api/alagamap/parameters.json";
 
-const params = overwrite(PARAMETERS_PATH, data => ({
+const authParams = overwrite(AUTH_PARAMETERS_PATH, data => ({
+  ...data,
+  googleAppIdUserPool: process.env.GOOGLE_CREDENTIALS_CLIENT_ID,
+  googleAppSecretUserPool: process.env.GOOGLE_CREDENTIALS_CLIENT_SECRET,
+}));
+
+const apiParams = overwrite(API_PARAMETERS_PATH, data => ({
   ...data,
   OneSignalAppId: process.env.ONE_SIGNAL_APP_ID,
   OneSignalRestKey: process.env.ONE_SIGNAL_REST_KEY,
 }));
 
-const teamProviderInfo = overwrite(TEAM_PROVIDER_INFO_PATH, data => ({
-  ...data,
-  dev: {
-    ...(data.dev || {}),
-    categories: {
-      ...((data.dev && data.dev.categories) || {}),
-      auth: {
-        ...((data.dev && data.dev.categories && data.dev.categories.auth) || {}),
-        cognito6170dcc7: {
-          ...((data.dev &&
-            data.dev.categories &&
-            data.dev.categories.auth &&
-            data.dev.categories.auth.cognito6170dcc7) ||
-            {}),
-          googleAppIdUserPool: process.env.GOOGLE_CREDENTIALS_CLIENT_ID,
-          googleAppSecretUserPool: process.env.GOOGLE_CREDENTIALS_CLIENT_SECRET,
-        },
-      },
-    },
-  },
-}));
-
 execSync("amplifyPush --simple", { stdio: "inherit" });
 
-teamProviderInfo();
-params();
+apiParams();
+authParams();
