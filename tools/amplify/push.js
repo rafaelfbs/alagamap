ae883df2d2bc6009c4862560f91e682db4d12735
Rafael Ferrari <me@rferrari.net>
2019-04-28 01:58:27 -0300

    update amplify cli and amplify push script

--- tools/amplify/push.js
+++ tools/amplify/push.js
@@ -1,56 +1,69 @@
-const { overwrite, exec } = require("./util");
+const { join, dirname } = require("path");
+const { exec } = require("./util");
 
-const AUTH_PARAMETERS_PATH = "./amplify/backend/auth/cognito6170dcc7/parameters.json";
-const API_PARAMETERS_PATH = "./amplify/backend/api/alagamap/parameters.json";
+const AMPLIFYDIR = require.resolve("@aws-amplify/cli/package.json");
+const AMPLIFYCMD = join(dirname(AMPLIFYDIR), "bin/amplify");
 
-overwrite(AUTH_PARAMETERS_PATH, data => ({
-  ...data,
-  googleAppIdUserPool: process.env.GOOGLE_CREDENTIALS_CLIENT_ID,
-  googleAppSecretUserPool: process.env.GOOGLE_CREDENTIALS_CLIENT_SECRET,
-}));
+const ENV = process.env.ENV || "dev";
 
-overwrite(API_PARAMETERS_PATH, data => ({
-  ...data,
-  OneSignalAppId: process.env.ONE_SIGNAL_APP_ID,
-  OneSignalRestKey: process.env.ONE_SIGNAL_REST_KEY,
-}));
+const AWSCLOUDFORMATION = JSON.parse(process.env.AWSCLOUDFORMATION);
+
+const STACKINFO = {
+  awscloudformation: AWSCLOUDFORMATION,
+  categories: {
+    api: {
+      alagamap: {
+        OneSignalAppId: process.env.ONE_SIGNAL_APP_ID,
+        OneSignalRestKey: process.env.ONE_SIGNAL_REST_KEY,
+      },
+    },
+    auth: {
+      cognito6170dcc7: {
+        hostedUIProviderCreds: JSON.stringify([
+          {
+            ProviderName: "Google",
+            client_id: process.env.GOOGLE_CREDENTIALS_CLIENT_ID,
+            client_secret: process.env.GOOGLE_CREDENTIALS_CLIENT_SECRET,
+          },
+        ]),
+      },
+    },
+  },
+};
+
+const AWSCONFIG = {
+  configLevel: "project",
+  useProfile: true,
+  profileName: "default",
+};
+
+const AMPLIFY = {
+  envName: ENV,
+};
+
+const PROVIDERS = {
+  awscloudformation: AWSCLOUDFORMATION,
+};
+
+const CATEGORIES = {
+  auth: {
+    googleAppIdUserPool: process.env.GOOGLE_CREDENTIALS_CLIENT_ID,
+    googleAppSecretUserPool: process.env.GOOGLE_CREDENTIALS_CLIENT_SECRET,
+  },
+};
 
 function escape(p) {
-  return `"${p.replace(/"/g, "\\\"")}"`;
+  return JSON.stringify(JSON.stringify(p));
 }
-async function main() {
-  const config = {
-    configLevel: "project",
-    useProfile: true,
-    profileName: "default",
-  };
-  const ENV = process.env.ENV || "dev";
-  const STACKINFO = process.env.STACKINFO;
-  const AWSCONFIG = JSON.stringify(config);
-  const AMPLIFY = JSON.stringify({
-    envName: ENV,
-  });
-  const PROVIDERS = JSON.stringify({
-    awscloudformation: config,
-  });
-  try {
-    if (!process.env.STACKINFO) {
-      console.log(`# Initializing new Amplify environment: ${ENV} (amplify init)`);
-      await exec(`amplify init --amplify ${escape(AMPLIFY)} --providers ${escape(PROVIDERS)} --yes`);
-      console.log(`# Environment ${ENV} details:`);
-      await exec(`amplify env get --name ${ENV}`);
-    } else {
-      console.log(`# Importing Amplify environment: ${ENV} (amplify env add)`);
-      await exec(
-        `amplify env add --name ${ENV} --config "${STACKINFO}" --awsInfo ${escape(AWSCONFIG)} --yes`,
-      );
-      console.log(`# Initializing existing Amplify environment: ${ENV} (amplify init)`);
-      await exec(`amplify init --amplify ${escape(AMPLIFY)} --providers ${escape(PROVIDERS)} --yes`);
-      console.log(`# Environment ${ENV} details:`);
-      await exec(`amplify env get --name ${ENV}`);
-    }
 
-    console.log(`# Done initializing Amplify environment: ${ENV}`);
+async function main() {
+  try {
+    console.log(`# Importing Amplify environment: ${ENV} (amplify env import)`);
+    await exec(`${AMPLIFYCMD} env import --name ${ENV} --config ${escape(STACKINFO)} --awsInfo ${escape(AWSCONFIG)} --yes`);
+    console.log(`# Initializing existing Amplify environment: ${ENV} (amplify init)`);
+    await exec(`${AMPLIFYCMD} init --amplify ${escape(AMPLIFY)} --providers ${escape(PROVIDERS)} --categories ${escape(CATEGORIES)} --yes`);
+    console.log(`# Environment ${ENV} details:`);
+    await exec(`${AMPLIFYCMD} env get --name ${ENV}`);
   } catch (code) {
     process.exit(code);
   }
